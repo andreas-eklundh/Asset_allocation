9a937e9e3edca69e623ec06f25a31ddc4d578ec6
andreas-eklundh <andreas.eklundh@hotmail.com>
2024-10-07 17:25:39 +0200

    Test of numpy and pandas

---
+++ Test.py
@@ -0,0 +1,17 @@
+### Test - basic packages:
+
+import numpy as np
+
+
+A = np.array([[1,0,0],
+              [0,2,0],
+              [0,0,1]])
+
+B = np.array([3,4,5])
+
+print(A @ B)
+
+
+import pandas as pd
+
+df = pd.DataFrame(A, columns=["A", "B", "C"])
