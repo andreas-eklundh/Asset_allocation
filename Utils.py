3f7779ea1132781b0e6b56459c41bbd3afc61cb5
andreas-eklundh <andreas.eklundh@hotmail.com>
2024-10-10 12:59:19 +0200

    Cleaning of yld and utils added

---
+++ Utils.py
@@ -0,0 +1,24 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def PV(C,Y,T):
+    pv = 0
+    t_j = [l for l in range(1,T)] 
+    for i in t_j:
+        pv +=  C / (1+Y)**(i) 
+    pv += C / (1+Y)**(T) +   1 / (1+Y)**T 
+    return pv
+
+def get_r(Yim1, Yi, Tm1, T):
+    pv1 = PV(Yim1,Yi,Tm1)
+    pv2 = PV(Yim1,Yim1,T)
+
+    r = pv1 / pv2 - 1
+
+    return r
+    
+
+    
+
