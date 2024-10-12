e4534eb303a906942df855a30fd32e05a36bd120
andreas-eklundh <andreas.eklundh@hotmail.com>
2024-10-12 10:12:10 +0200

    initial pf analysis

--- Utils.py
+++ Utils.py
@@ -24,5 +24,43 @@ def get_r(Yim1, Yi, Tm1, T):
     return r
     
 
+# Functionality for finding minimum variance PF traditionally.
+
+def min_var(mu,sigma, mu_target):
+    sigma_inv = np.linalg.inv(sigma)
+    o = np.ones(len(mu))
+    a = mu @ sigma_inv @ mu
+    b = mu @ sigma_inv @ o
+    c = o  @ sigma_inv @ o
+    A = np.array([[a, b],
+                  [b,c]])
+    A_inv = np.linalg.inv(A)
+    w_target = sigma_inv @ np.array([mu,o]).T @ A_inv @ np.array([mu_target, 1])
+
+    std = np.sqrt(w_target @ sigma @ w_target)
+
+    return w_target, std
+
+# Functionality for finding minimum variance PF with risk free asset. Leverage allowed.
+def min_var_rf(mu,sigma, mu_target):
+    '''
+    Note: Input exess return.
+    '''
+    sigma_inv = np.linalg.inv(sigma)
+    o = np.ones(len(mu))
+    w_target = mu_target * sigma_inv @ mu / (mu.T @ sigma_inv @ mu)
+
+    std = np.sqrt(w_target @ sigma @ w_target)
+
+    w0 = 1 - w_target @ o 
+
+    return w0, w_target, std
+
+def risk_parity_fun(w,sigma):
+    std = np.sqrt(w @ sigma @ w)
+    N  = len(w)
+    w_rp = std**2 / ((sigma @ w) * N)
+    fun = np.sum((w - w_rp)**2)
     
+    return fun
 
