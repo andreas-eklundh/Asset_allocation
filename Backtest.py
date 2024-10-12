b28b9a82451b6025ad7a982d8805f0bd4aa38a29
andreas-eklundh <andreas.eklundh@hotmail.com>
2024-10-12 15:39:19 +0200

    fixing backtest_naive

--- Backtest.py
+++ Backtest.py
@@ -4,28 +4,30 @@ import pandas as pd
 import Utils as u
 
 
-def backtest_naive(data,mu_target):
-    # Note when specifying data, then also period should be specified. 
-    mu = np.mean([data["10YrReturns"],data["Market Return"]],axis=1)
-    sigma = np.cov([data["10YrReturns"],data["Market Return"]])
-    mu0 = np.mean(data["RF"])
-    mu_e = np.mean([data["10YrReturns"] - data["RF"],data["Market Return"]-data["RF"]],axis=1)
-    sigma_e = np.cov([data["10YrReturns"] - data["RF"],data["Market Return"] - data["RF"]])
+def backtest_naive(ind,mu_target):
+    # Note when specifying ind, then also period should be specified. 
+    mu = np.mean([ind['10YrReturns'],ind['Market Return']],axis=1)
+    sigma = np.cov([ind['10YrReturns'],ind['Market Return']])
+    mu0 = np.mean(ind['RF'])
+    mu_e = np.mean([ind['10YrReturns'] - ind['RF'],ind['Market Return']-ind['RF']],axis=1)
+    sigma_e = np.cov([ind['10YrReturns'] - ind['RF'],ind['Market Return'] - ind['RF']])
     mu_target_e = mu_target - mu0
     # Retrieve weigths
     w = u.get_weights(mu,sigma, mu_target,mu_e, sigma_e, mu_target_e)
     w_rf =  [1 - i @ np.ones(len(i)) for i in w]
-    w_method = ["R_40/60", "R_MV", "R_MVL", "R_RP", "R_RPL"]
+    w_method = ['R_40/60', 'R_MV', 'R_MVL', 'R_RP', 'R_RPL']
     idx = [i for i in range(0,len(w_method))]
+    out = ind.copy()
     # We do it in returns
     for i,j in zip(idx,w_method):
-        data[j] = 1 + (w_rf[i] * data["RF"] + w[i][0] * data["10YrReturns"] + w[i][1] * data["Market Return"])
+        out[j] = 1 + (w_rf[i] * out['RF'] + w[i][0] * out['10YrReturns'] + w[i][1] * out['Market Return'])
     # Now, only the first obs has correct weights used. 
     # From now on accounting of increase (decrease) in wealth must be accounted for
     for i,j in zip(idx,w_method):
-        for k in range(1,len(data["RF"] )):
-            w0, w1, w2 = w_rf[i] / data[j][k-1], w[i][0]/ data.loc[k-1, j], w[i][1] / data.loc[k-1, j]
-            data.loc[k, j] = (1 +  w0 * data.loc[k, "RF"] + w1 * data.loc[k, "10YrReturns"] +
-                               w2 * data.loc[k, "Market Return"])*data.loc[k-1, j]
+        for k in range(1,len(out['RF'] )):
+            km1 = k - 1
+            w0, w1, w2 = w_rf[i] / out.loc[km1, j], w[i][0]/ out.loc[km1, j], w[i][1] / out.loc[km1, j]
+            out.loc[k, j] = (1 +  w0 * out.loc[k, 'RF'] + w1 * out.loc[k, '10YrReturns'] +
+                               w2 * out.loc[k, 'Market Return'])*out.loc[km1, j]
 
-    return data, w0, w1, w2 
+    return out, w0, w1, w2 
--- Test.py
+++ Test.py
@@ -1,24 +1,43 @@
-### Test - basic packages:
-
 import numpy as np
-
-
-A = np.array([[1,0,0],
-              [0,2,0],
-              [0,0,1]])
-
-B = np.array([3,4,5])
-
-print(A @ B)
-
-
+import matplotlib.pyplot as plt
 import pandas as pd
+import Utils
+from scipy.optimize import minimize 
+import Backtest as bt   
+import matplotlib.dates as mdates
 
-df = pd.DataFrame(A, columns=["A", "B", "C"])
+mu_target = 75 / 10000 # Target
+bond = pd.read_csv("Data_clean/bond_returns.csv")
+stock = pd.read_csv("Data_clean/6_Portfolios_ME_Prior_12_2_returns.csv")
+bond, stock =bond[["Date", "10YrReturns"]], stock[["Date", "Market Return"]]
+stock["Market Return"] = stock["Market Return"] /100  
+RF = pd.read_csv("Data_clean/FF_cleaned.csv")
+data = pd.merge(bond,stock, how='left', on = "Date")
 
-df = df.copy() + 2
+data = pd.merge(data.copy(),RF, 'left',on = "Date" )
+data["RF"] = data["RF"] /100 # assumed this must hold
 
+# test of backtest. 
+data1 = data.copy()
+# Now perform the comparison
+data1 = data1[pd.Timestamp('2010-01-31') <=pd.to_datetime(data1["Date"])]
 
-print(df)
+test2,w0t,w1t,w2t = bt.backtest_naive(ind=data1, mu_target=mu_target)
 
-print("Hello World!")
+time = pd.date_range(data1["Date"][0],data1["Date"][len(data["Date"]) -1 ], freq = 'ME')
+fig, ax = plt.subplots()
+ax.plot(time,data1["R_40/60"], label="R of 40/60")
+ax.plot(time,data1["R_MV"], label="R of Mean-Var")
+ax.plot(time,data1["R_MVL"], label="R of Mean-Var Leveraged")
+ax.plot(time,data1["R_RP"], label="R of Risk Parity")
+ax.plot(time,data1["R_RPL"], label="R of Risk Parity Leveraged")
+ax.xaxis.set_major_locator(mdates.YearLocator(5))  # Set a tick at the start of every year
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+plt.legend()
+plt.grid()
+plt.xticks(rotation=45, ha='right')
+
+plt.xlabel("Time in months from start date")
+plt.ylabel("Cumulative return")
+plt.title("Subperiod:")
+plt.show()
